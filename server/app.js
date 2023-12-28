15c6d628fe0c857fc80f86cb02818d24e1f63a17
athulws <athulraj@weamse.in>
2023-12-28 11:30:33 +0530

    details added using postman

--- server/app.js
+++ server/app.js
@@ -2,11 +2,26 @@ require("dotenv").config();
 const express = require("express")
 const app = express();
 const mongoose = require("mongoose");
-require("./db/conn")
-require("./Models/userSchema")
-
+require("./db/conn");
+require("./Models/userSchema");
+const cors = require("cors");
+const router = require("./routes/router");
 
 const port = 8003;
+
+//ith cors cheyyumbol
+
+app.use(cors());
+app.use(express.json()); // nammude data
+
+//ith cors cheyyumbol
+
+
+//ith router ne
+
+app.use(router);
+
+//ith router ne
 
 app.listen(port,()=>{
     console.log(`server is start port number ${port}`);
